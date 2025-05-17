bd307a9a65a2d2464db72ad085e76162d5500a2f
Tomasz Kawalec <tkawalec369@gmail.com>
2025-05-17 15:05:18 +0200

    Add Trie.inorder_iterator method, and add another unit test for it

--- src/utils.zig
+++ src/utils.zig
@@ -95,6 +95,56 @@ pub fn Trie(comptime T: type) type {
             element: ?*T,
         };
 
+        pub const InorderIterator = struct {
+            pub const Pair = struct {
+                key: []const u8,
+                value: *T,
+            };
+
+            const StackFrame = struct {
+                node: *Node,
+                elem_checked: bool,
+                children_checked: i16,
+            };
+
+            trie: *This,
+            nodes_stack: std.ArrayList(StackFrame),
+            key_stack: std.ArrayList(u8),
+
+            pub fn deinit(self: *InorderIterator) void {
+                self.nodes_stack.deinit();
+                self.key_stack.deinit();
+            }
+
+            pub fn next(self: *InorderIterator) !?Pair {
+                const curr_frame = &self.nodes_stack.items[self.nodes_stack.items.len - 1];
+                if (curr_frame.elem_checked == false) {
+                    curr_frame.elem_checked = true;
+                    if (curr_frame.node.element) |elem_nonull| {
+                        return .{ .key = self.key_stack.items, .value = elem_nonull };
+                    }
+                }
+                while (curr_frame.children_checked < 256) {
+                    curr_frame.children_checked += 1;
+                    if (curr_frame.node.children[@intCast(curr_frame.children_checked - 1)]) |child_nonull| {
+                        try self.key_stack.append(@intCast(curr_frame.children_checked - 1));
+                        try self.nodes_stack.append(.{
+                            .node = child_nonull,
+                            .elem_checked = false,
+                            .children_checked = 0,
+                        });
+                        return self.next();
+                    }
+                }
+                _ = self.nodes_stack.pop();
+                _ = self.key_stack.pop();
+                if (self.nodes_stack.items.len == 0) {
+                    return null;
+                }
+                return self.next();
+            }
+        };
+
         root: *Node,
         allocator: std.mem.Allocator,
 
@@ -118,6 +168,19 @@ pub fn Trie(comptime T: type) type {
         pub fn get(self: *This, key: []const u8, create_if_absent: bool) !?*T {
             return get_internal(self, key, self.root, create_if_absent);
         }
+        pub fn inorder_iterator(self: *This) !InorderIterator {
+            var nodes_stack = std.ArrayList(InorderIterator.StackFrame).init(self.allocator);
+            try nodes_stack.append(.{
+                .node = self.root,
+                .elem_checked = false,
+                .children_checked = 0,
+            });
+            return .{
+                .trie = self,
+                .nodes_stack = nodes_stack,
+                .key_stack = .init(self.allocator),
+            };
+        }
 
         fn deinit_node(self: *This, node: *Node) void {
             for (node.children) |child| {
@@ -126,6 +189,14 @@ pub fn Trie(comptime T: type) type {
                 }
             }
             if (node.element) |element_nonull| {
+                if (std.meta.hasMethod(T, "unref")) {
+                    self.value.unref();
+                    std.debug.print("Unrefing value inside Trie\n", .{});
+                }
+                if (std.meta.hasMethod(T, "deinit")) {
+                    self.value.deinit();
+                    std.debug.print("Deiniting value inside Trie\n", .{});
+                }
                 self.allocator.destroy(element_nonull);
             }
             self.allocator.destroy(node);
@@ -176,4 +247,40 @@ test "Trie 1" {
 
     try std.testing.expectEqual(null, (try trie.get("array", false)));
     try std.testing.expectEqual(null, (try trie.get("banana", false)));
+
+    var iter = try trie.inorder_iterator();
+    defer iter.deinit();
+    while (try iter.next()) |elem| {
+        std.debug.print("\"{s}\": {d}\n", .{ elem.key, elem.value.* });
+    }
+}
+
+test "Trie 2" {
+    var trie = try Trie(i32).init(std.testing.allocator);
+    defer trie.deinit();
+
+    (try trie.get("abc", true)).?.* = 873;
+    (try trie.get("abcdef", true)).?.* = 3;
+    (try trie.get("Hello world!", true)).?.* = -5;
+    (try trie.get("The quick brown fox jumps over the lazy dog.", true)).?.* = 100;
+
+    var iter = try trie.inorder_iterator();
+    defer iter.deinit();
+    // while (try iter.next()) |elem| {
+    //     std.debug.print("\"{s}\": {d}\n", .{ elem.key, elem.value.* });
+    // }
+    var p = try iter.next();
+    try std.testing.expectEqualStrings("Hello world!", p.?.key);
+    try std.testing.expectEqual(-5, p.?.value.*);
+    p = try iter.next();
+    try std.testing.expectEqualStrings("The quick brown fox jumps over the lazy dog.", p.?.key);
+    try std.testing.expectEqual(100, p.?.value.*);
+    p = try iter.next();
+    try std.testing.expectEqualStrings("abc", p.?.key);
+    try std.testing.expectEqual(873, p.?.value.*);
+    p = try iter.next();
+    try std.testing.expectEqualStrings("abcdef", p.?.key);
+    try std.testing.expectEqual(3, p.?.value.*);
+    p = try iter.next();
+    try std.testing.expectEqual(null, p);
 }
