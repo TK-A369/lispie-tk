642088ea36fd8809ff20a5743ab949bed4466f37
Tomasz Kawalec <tkawalec369@gmail.com>
2025-05-17 13:40:49 +0200

    Implement Trie data structure, intended for use as key-value map with string keys; also write a simple unit test for it

--- src/utils.zig
+++ src/utils.zig
@@ -85,3 +85,95 @@ test "RefCount 2" {
     try std.testing.expectEqual(3, rc2.value.*);
     rc2.unref();
 }
+
+pub fn Trie(comptime T: type) type {
+    return struct {
+        const This = @This();
+
+        pub const Node = struct {
+            children: [256]?*Node,
+            element: ?*T,
+        };
+
+        root: *Node,
+        allocator: std.mem.Allocator,
+
+        pub fn init(allocator: std.mem.Allocator) !This {
+            var children: [256]?*Node = undefined;
+            for (&children) |*child| {
+                child.* = null;
+            }
+            const root_ptr = try allocator.create(Node);
+            root_ptr.* = .{ .children = children, .element = null };
+            return .{
+                .root = root_ptr,
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: *This) void {
+            self.deinit_node(self.root);
+        }
+
+        pub fn get(self: *This, key: []const u8, create_if_absent: bool) !?*T {
+            return get_internal(self, key, self.root, create_if_absent);
+        }
+
+        fn deinit_node(self: *This, node: *Node) void {
+            for (node.children) |child| {
+                if (child) |child_nonull| {
+                    self.deinit_node(child_nonull);
+                }
+            }
+            if (node.element) |element_nonull| {
+                self.allocator.destroy(element_nonull);
+            }
+            self.allocator.destroy(node);
+        }
+
+        fn get_internal(self: *This, key: []const u8, node: *Node, create_if_absent: bool) !?*T {
+            if (key.len > 0) {
+                var child = node.children[key[0]];
+                // I believe that right now, there's no better syntax for this
+                if (child) |_| {} else {
+                    if (create_if_absent) {
+                        child = try self.allocator.create(Node);
+                        for (&child.?.children) |*subchild| {
+                            subchild.* = null;
+                        }
+                        child.?.element = null;
+
+                        node.children[key[0]] = child;
+                    } else {
+                        return null;
+                    }
+                }
+                return self.get_internal(key[1..], child.?, create_if_absent);
+            } else {
+                if (create_if_absent) {
+                    if (node.element) |_| {} else {
+                        node.element = try self.allocator.create(T);
+                    }
+                }
+                return node.element;
+            }
+        }
+    };
+}
+
+test "Trie 1" {
+    var trie = try Trie(i32).init(std.testing.allocator);
+    defer trie.deinit();
+
+    (try trie.get("abc", true)).?.* = 73;
+    (try trie.get("avocado", true)).?.* = 987;
+
+    try std.testing.expectEqual(73, (try trie.get("abc", false)).?.*);
+    try std.testing.expectEqual(987, (try trie.get("avocado", false)).?.*);
+
+    try std.testing.expectEqual(987, (try trie.get("avocado", true)).?.*);
+    try std.testing.expectEqual(73, (try trie.get("abc", true)).?.*);
+
+    try std.testing.expectEqual(null, (try trie.get("array", false)));
+    try std.testing.expectEqual(null, (try trie.get("banana", false)));
+}
